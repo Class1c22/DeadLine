d1137cc0b57c9e71d132457d54bae386ffc2c9dd
Class1c <162556359+Class1c22@users.noreply.github.com>
2026-09-26 11:40:18 +0300

    дебаг

--- Assets/scripts/ui/menu/imageButton.cs
+++ Assets/scripts/ui/menu/imageButton.cs
@@ -9,6 +9,12 @@ public class imageButton : MonoBehaviour, IPointerClickHandler
 
     public void OnPointerClick(PointerEventData eventData)
     {
+
+        Debug.Log($"[imageButton] Клік! Кількість підписників onClick: {onClick.GetPersistentEventCount()}");
         onClick.Invoke();
+        Debug.Log("[imageButton] onClick.Invoke() виконано");
+
     }
+
+    
 }
